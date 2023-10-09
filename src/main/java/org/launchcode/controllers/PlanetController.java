157998b4f1ffc0fc18097484bbe22f8f76d9a083
Bridget <bridget.carlile@gmail.com>
2023-10-08 20:45:37 -0500

    attempted

--- src/main/java/org/launchcode/controllers/PlanetController.java
+++ src/main/java/org/launchcode/controllers/PlanetController.java
@@ -1,13 +1,19 @@
 package org.launchcode.controllers;
 
+import data.Planets;
+import models.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.text.AttributedString;
 
 @Controller
 public class PlanetController {
 
     @GetMapping
-    public String displayIndex() {
+    public String displayIndex(Model, "model") {
+
+        model.addAttribute("planets", Planets.values());
         return "index";
     }
 }
